f93b9887ce6e25172bd71730841c561486c71d02
VoNhatSinh <nhatsinh18@gmail.com>
2016-06-17 00:02:12 +0700

    Update trang product detail

---
+++ HTML/js/slide-image.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        autoplay:false,
+        autoplayHoverPause: true,
+        smartSpeed: 450,
+        center:true,
+        loop: true,
+        //margin:10,
+        dots: false,
+        dotsEach: 1,
+        nav: false,
+       // navClass: ['owl-prev fa fa-angle-left', 'owl-next fa fa-angle-right'],
+       
+        responsive: {
+            0: { items: 1 },
+            768: { items: 1 },
+            980: { items: 1 }
+        }
+    });
+});
