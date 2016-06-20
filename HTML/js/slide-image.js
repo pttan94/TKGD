7fde52be55d3c7fbf23df2bc1017c9ffbd994a90
VoNhatSinh <nhatsinh18@gmail.com>
2016-06-20 11:46:23 +0700

    Update product detail

--- HTML/js/slide-image.js
+++ HTML/js/slide-image.js
@@ -1,14 +1,17 @@
 ﻿$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        autoplay:false,
+        item:1,
+        autoplay:true,
         autoplayHoverPause: true,
         smartSpeed: 450,
         center:true,
-        loop: true,
+        loop: false,
         //margin:10,
         dots: false,
         dotsEach: 1,
         nav: false,
+        URLhashListener: true,
+        startPosition: '#zero',
        // navClass: ['owl-prev fa fa-angle-left', 'owl-next fa fa-angle-right'],
        
         responsive: {
